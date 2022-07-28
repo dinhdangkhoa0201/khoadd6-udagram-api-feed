7ae9dc3c3c80850409a8c5dc8228f9bcad41ba5b
KhoaDD6 <dinhdangkhoa0201@gmail.com>
2022-07-28 21:55:12 +0700

    Modify deployDocker

--- deployDocker.sh
+++ deployDocker.sh
@@ -1,4 +1,4 @@
 docker build -t udagram-api-feed .
-#docker tag udagram-api-feed dinhdangkhoa0201/udagram-api-feed
-#echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-#docker push dinhdangkhoa0201/udagram-api-feed
+docker tag udagram-api-feed dinhdangkhoa0201/udagram-api-feed
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push dinhdangkhoa0201/udagram-api-feed
